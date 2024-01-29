e7ef94a848413c6446a2491a352b3f1c217a7b73
iivanov <selivanov@realx.tech>
2024-01-29 13:21:52 +0300

    test user commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,35 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: "Animals",
-  description: "for pets",
-};
+	title: 'Animals',
+	description: 'for pets',
+	robots: {
+		index: false,
+		follow: true,
+		nocache: true,
+		googleBot: {
+			index: true,
+			follow: false,
+			noimageindex: true,
+			'max-video-preview': -1,
+			'max-image-preview': 'large',
+			'max-snippet': -1,
+		},
+	},
+}
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode
 }>) {
-  return (
-    <html lang="ru">
-      <body className={inter.className}>{children}</body>
-    </html>
-  );
+	return (
+		<html lang='ru'>
+			<body className={inter.className}>{children}</body>
+		</html>
+	)
 }
+
